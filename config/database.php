35b5055e198921b7925da0bf6d67e9227394bb41
Gustavo Souza <gustavottsouza@gmail.com>
2024-04-17 10:52:19 -0300

    Change test database to memory

--- config/database.php
+++ config/database.php
@@ -39,6 +39,12 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'sqlite_testing' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => ''
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
